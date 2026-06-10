2f426961dc6d3c94847b06d9fc44260d77a6acfd
MASTASHADEY <70362031+MASTASHADEY@users.noreply.github.com>
2026-06-09 20:08:26 -0400

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,31 +1,37 @@
-#!/usr/bin/env python3
-import rospy
-from geometry_msgs.msg import Point
-
-def send_goal():
-    rospy.init_node('test_goal_publisher', anonymous=True)
-    
-    # Connect directly to the topic your Task 8 node is listening to
-    pub = rospy.Publisher('/goal_task_space', Point, queue_size=10)
-    
-    # Wait 1 second to ensure the network connection is fully established
-    rospy.sleep(1.0) 
-    
-    # Package the safe coordinates
-    msg = Point()
-    msg.x = 0.15
-    msg.y = 0.0
-    msg.z = 0.10
-    
-    rospy.loginfo(f"Sending coordinates: X={msg.x}, Y={msg.y}, Z={msg.z}")
-    pub.publish(msg)
-    
-    # Wait 1 second before shutting down so the message isn't dropped
-    rospy.sleep(1.0)
-    rospy.loginfo("Message sent!")
-
-if __name__ == '__main__':
-    try:
-        send_goal()
-    except rospy.ROSInterruptException:
-        pass
+rostopic pub -1 /arm_controller/follow_joint_trajectory/goal control_msgs/FollowJointTrajectoryActionGoal "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: ''
+goal_id:
+  stamp:
+    secs: 0
+    nsecs: 0
+  id: ''
+goal:
+  trajectory:
+    header:
+      seq: 0
+      stamp:
+        secs: 0
+        nsecs: 0
+      frame_id: ''
+    joint_names:
+    - joint1
+    - joint2
+    - joint3
+    - joint4
+    points:
+    - positions: [0.0, -0.2, 0.2, 0.0]
+      velocities: [0.0, 0.0, 0.0, 0.0]
+      accelerations: [0.0, 0.0, 0.0, 0.0]
+      effort: []
+      time_from_start:
+        secs: 3
+        nsecs: 0
+  path_tolerance: []
+  goal_tolerance: []
+  goal_time_tolerance:
+    secs: 0
+    nsecs: 0"
